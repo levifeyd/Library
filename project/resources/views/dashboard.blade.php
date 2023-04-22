e09b00c0ca721df4ec20f322de193751018347e1
levifeyd <kuleshov-as@mail.ru>
2023-04-22 14:23:12 +0400

    refactor dashbord view, create ul of books, refactor welcome view

--- project/resources/views/dashboard.blade.php
+++ project/resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Добро пожаловать в электронную библиотеку') }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,21 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    {{ __("Список книг") }}
+                </div>
+                <div class="container mt-6">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <a href="#" class="btn btn-success mb-4">Add new post</a>
+                            <ul id="requests-list">
+                                <div class="card mb-4">
+                                    <h8 class="card-header">Заявка №</h8>
+                                    <h8 class="card-header">Категория:</h8>
+                                    <h8 class="card-header">Статус заявки:</h8>
+                                </div>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
